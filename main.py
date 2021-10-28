0b48037e91fdff050febab8c402123b44e89f507
AJunkman <1938059355@qq.com>
2021-10-28 18:02:32 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 from multiprocessing.dummy import Pool as ThreadPool
-import threading
+
 threadNum = 5
 nums = [1, 2, 3, 4, 5]
 def start(str):
     print('osuSim%s starting...'%(str))
     os.system('python3 osuSim%s.py'%(str))
-
+    
 pool = ThreadPool(threadNum)
 pool.map(start, nums)
 pool.close()
 pool.join()
+
