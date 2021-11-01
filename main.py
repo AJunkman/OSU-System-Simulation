c343d0b3540fadeef2b073be545d222610322d3c
weiwang0368@gmail.com <weiwang0368@gmail.com>
2021-11-01 19:43:10 +0800

    show how to push code to github

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ nums = [1, 2, 3, 4, 5]
 def start(str):
     print('osuSim%s starting...'%(str))
     os.system('python3 osuSim%s.py'%(str))
-    
+
 pool = ThreadPool(threadNum)
 pool.map(start, nums)
 pool.close()
 pool.join()
 
+test()
