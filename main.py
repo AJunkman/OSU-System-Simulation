87724051116b31af70e958176c5d25c48298207e
lth6696 <lth9804@bupt.edu.cn>
2021-11-05 20:23:28 +0800

    independent log

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ nums = [1, 2, 3, 4, 5]
 thd = []
 for str in nums:
     logging.info('osuSim%s starting...'%(str))
-    thd.append(threading.Thread(target=osuSim.sim_run, args=("topologies/osu{}.cfg".format(str),), name='OSU{}'.format(str)))
+    thd.append(threading.Thread(target=osuSim.sim_run,
+                                args=("topologies/osu{}.cfg".format(str),),
+                                name='OSU{}'.format(str)))
     thd[str-1].start()
