07c4fd51eed64d34e3f392f31b93f2cc773fba5c
lth6696 <lth9804@bupt.edu.cn>
2021-11-05 10:55:20 +0800

    update

--- ospf.py
+++ ospf.py
@@ -1,29 +1,27 @@
-import sys
-sys.path.append(r'/home/osu-sim/share/osu-sim')
 import dijkstra
 
 
-TIME_SCALE = 20 # 1 minute (60 seconds) is to 3 seconds (60 / 3 = 20)
+TIME_SCALE = 20     # 1 minute (60 seconds) is to 3 seconds (60 / 3 = 20)
 
 
 def _scale_time(minutes):
-    return (60.0 * minutes / TIME_SCALE)
+    return 60.0 * minutes / TIME_SCALE
 
 
-BANDWIDTH_BASE = 100000000 # 100 Mbps
-HELLO_INTERVAL = 10 # 10 seconds
-DEAD_INTERVAL = 4 * HELLO_INTERVAL # typical value is 4 times the HELLO_INTERVAL
-AGE_INTERVAL = _scale_time(1) # 1 minute
-LS_REFRESH_TIME = _scale_time(30) # 30 minutes
-MAX_AGE = _scale_time(60) # 1 hour
+BANDWIDTH_BASE = 10e7           # 100 Mbps
+HELLO_INTERVAL = 10             # 10 seconds
+DEAD_INTERVAL = 4 * HELLO_INTERVAL  # 为 HELLO_INTERVAL 的四倍
+AGE_INTERVAL = _scale_time(1)   # 1 minute
+LS_REFRESH_TIME = _scale_time(30)   # 30 minutes - LSA刷新时间
+MAX_AGE = _scale_time(60)       # 1 hour - LSA老化时间
 
 
 class LinkStatePacket(object):
 
     def __init__(self, osu_id, age, seq_no, networks, tlv: dict):
-    # 报文首部
+        # 报文首部
         self.adv_osu = osu_id
-        self.age = age
+        self.age = age          # 老化时间范围：0-3600秒
         self.seq_no = seq_no
 
         # RTR_LSA 净荷
@@ -33,7 +31,8 @@ class LinkStatePacket(object):
         self.tlv = tlv
 
     def __repr__(self):
-        stat = '\nADV Osu: %s\nAge: %d\nSeq No.: %d\nNetworks: %s\nTLV: %s\n' % (self.adv_osu, self.age, self.seq_no, self.networks, self.tlv)
+        stat = "Adv OSU: {}, Age: {}, Seq: {}, Networks: {}, TLV: {}"\
+            .format(self.adv_osu, self.age, self.seq_no, self.networks, self.tlv)
         return stat
 
     def init_tlv(self, iface_name, lcl_id, rmt_id, max_bw, ava_bw, use_bw, av_delay=None):
@@ -57,7 +56,8 @@ class HelloPacket(object):
         self.netmask = netmask
         self.seen = seen
 
-	# Type-Length-Value结构
+
+# Type-Length-Value结构
 class TLV:
 
     def __init__(self, tpe, val):
@@ -72,14 +72,19 @@ class Link_TLV(TLV):
         self.type = tpe
         self.val = {}
 
-    # 初始化链路标识TLV
-    def init_lrrid_tlv(self, lcl_id: str, rmt_id: str):
-        self.val['10'] = (lcl_id, rmt_id)
-
     # 初始化最大带宽子TLV - TE隧道可以使用的带宽上限值（通常接口带宽）
     def init_max_bw_tlv(self, max_bw: float):
         # 带宽单位 Byte/s
         self.val['6'] = max_bw
+
+    # 初始化链路标识TLV
+    def init_lrrid_tlv(self, lcl_id: str, rmt_id: str):
+        self.val['10'] = (lcl_id, rmt_id)
+
+    # 初始化平均时延子TLV
+    def init_av_delay(self, av_delay: float):
+        # 时延单位 微妙
+        self.val['27'] = av_delay
 
     # 初始化可用带宽子TLV
     # 隧道可分配带宽，初值默认为最大带宽
@@ -87,22 +92,30 @@ class Link_TLV(TLV):
         self.val['32'] = ava_bw
 
     # 初始化占用带宽子TLV
-    # 提供给隧道后剩余的带宽，初值默认为最大可预留带宽
+    # 提供给隧道后剩余的带宽，初值默认为0
     def init_use_bw_tlv(self, use_bw: float):
         self.val['33'] = use_bw
-
-    # 初始化平均时延子TLV
-    def init_av_delay(self, av_delay: float):
-        # 时延单位 微妙
-        self.val['27'] = av_delay
 
 
 class Database(dict):
 
+    def _flush(self):
+        """Flush old entries"""
+        flushed = []
+        for osu_id in self:
+            if self[osu_id].age > MAX_AGE:
+                flushed.append(osu_id)
+        map(self.pop, flushed)      # 将对应flushed内的LSA记录删除
+        return flushed
+
     def insert(self, lsa):
-        """Returns True if LSA was added/updated"""
-        if lsa.adv_osu not in self or \
-           lsa.seq_no > self[lsa.adv_osu].seq_no:
+        """
+        插入分如下情况：
+        1 - 本地无通告记录
+        2 - LSA更新（比较序列号大小）
+        返回布尔值
+        """
+        if lsa.adv_osu not in self or lsa.seq_no > self[lsa.adv_osu].seq_no:
             self[lsa.adv_osu] = lsa
             return True
         else:
@@ -113,20 +126,13 @@ class Database(dict):
         if osu_id in self:
             del self[osu_id]
 
-    def flush(self):
-        """Flush old entries"""
-        flushed = []
-        for osu_id in self:
-            if self[osu_id].age > MAX_AGE:
-                flushed.append(osu_id)
-        map(self.pop, flushed)
-        return flushed
-
     def update(self):
-        """Update LSDB by aging the LSAs and flushing expired LSAs"""
+        """
+        通过老化LSA和刷新过期的LSA来更新LSDB
+        """
         for adv_osu in self:
             self[adv_osu].age += 1
-        return self.flush()
+        return self._flush()
 
     def get_shortest_paths(self, osu_id):
         """Return a list of shortest paths from osu_id to all other nodes"""
