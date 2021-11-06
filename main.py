6d6fe957dc0fc5e79c3aca3b8f840130ed093b2e
AJunkman <1938059355@qq.com>
2021-11-06 17:44:41 +0800

    delete starting

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ log.config_root_logger()
 nums = [1, 2, 3, 4, 5]
 thd = []
 for str in nums:
-    logging.info('osuSim%s starting...'%(str))
     thd.append(threading.Thread(target=osuSim.sim_run,
                                 args=("topologies/osu{}.cfg".format(str),),
                                 name='OSU{}'.format(str)))
